31a73a8e5e184f7f0b6e1d17bc0d9bf22388952b
Guille <g.granado.g@gmail.com>
2022-01-26 18:34:21 +0100

    create LinkQuery and remove resolvers file (#10)

--- graphql/resolvers.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from "./context"
-
-export const resolvers = {
-    Query: {
-        links: async (_parent: any, _args: any, context: Context) => 
-            await context.prisma.link.findMany()
-    }
-}
