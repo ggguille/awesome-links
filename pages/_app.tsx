9dfc7471587d67cd8740166c652436eed51ba6b4
Guille <g.granado.g@gmail.com>
2022-01-28 19:30:36 +0100

    Setting up apollo client nextjs (#11)
    * add apollo client dependency
    
    * create apollo client instance
    
    * add apollo provider
    
    * fetch data using apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@ import '../styles/tailwind.css'
 import { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 
+import { ApolloProvider } from '@apollo/client'
+import apolloClient from '../lib/apollo'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={apolloClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   )
 }
 
